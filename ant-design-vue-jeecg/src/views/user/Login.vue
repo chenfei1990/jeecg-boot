7a5869acad46dd4c7c1b3c7d1a98658ce6e00c8d
chenfei <2868924424@qq.com>
2020-11-03 16:48:00 +0800

    yy

--- ant-design-vue-jeecg/src/views/user/Login.vue
+++ ant-design-vue-jeecg/src/views/user/Login.vue
@@ -108,8 +108,8 @@
       <div class="user-login-other">
         <span>其他登录方式</span>
         <!-- <a @click="onThirdLogin('github')" title="github"><a-icon class="item-icon" type="github"></a-icon></a> -->
-        <a @click="onThirdLogin('wechat_enterprise')" title="企业微信"><a-icon class="item-icon" type="wechat"></a-icon></a>
-        <!-- <a @click="onThirdLogin('dingtalk')" title="钉钉"><a-icon class="item-icon" type="dingding"></a-icon></a> -->
+        <!-- <a @click="onThirdLogin('wechat_enterprise')" title="企业微信"><a-icon class="item-icon" type="wechat"></a-icon></a> -->
+        <a @click="onThirdLogin('dingtalk')" title="钉钉"><a-icon class="item-icon" type="dingding"></a-icon></a>
       </div>
     </a-form>
 
